97c6b9c1317830f2eb84466ea1dfd3de64ee1d86
MarielaBarba <77119353+MarielaBarba@users.noreply.github.com>
2021-04-17 11:50:44 -0500

    actualizado con restricciones

--- tableroUsuarios.php
+++ tableroUsuarios.php
@@ -1,6 +1,11 @@
 <?php
 include('includes/menu.php');
 include('includes/header.php');
+
+if($_SESSION["tipoUsuario"]!="Administrador"){
+  echo '<script type="text/javascript">alert("Inicie sesión nuevamente.");</script>';
+  echo '<script type="text/javascript">onload=window.location="index.php";</script>';
+}else{
 ?>
 
 <style>
@@ -502,3 +507,7 @@ $(document).on("click", ".opcFilTipoUsu", function () {
 });
 
 </script>
+
+<?php
+} //cierre llave else SESSION
+?>
