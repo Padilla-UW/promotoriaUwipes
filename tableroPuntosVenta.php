25a8cb3c255181fc8db3302453f2015fd1048a1a
MarielaBarba <77119353+MarielaBarba@users.noreply.github.com>
2021-04-17 11:53:17 -0500

    actualizacion restricciones

--- tableroPuntosVenta.php
+++ tableroPuntosVenta.php
@@ -2,6 +2,11 @@
 session_start();
 include('includes/menu.php');
 include('includes/header.php');
+
+if($_SESSION["tipoUsuario"]!="Administrador"){
+  echo '<script type="text/javascript">alert("Inicie sesión nuevamente.");</script>';
+  echo '<script type="text/javascript">onload=window.location="index.php";</script>';
+}else{
 ?>
 
 <style>
@@ -452,3 +457,7 @@ function getVendedorFiltro(filtro) {
 }
 
 </script>
+
+<?php
+} //cierre llave else SESSION
+?>
