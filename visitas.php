9cf7e651b3adf56244ff380239b8d3f0bd09aaca
MarielaBarba <77119353+MarielaBarba@users.noreply.github.com>
2021-04-17 11:55:32 -0500

    actualizacion restricciones

--- visitas.php
+++ visitas.php
@@ -2,6 +2,11 @@
 session_start();
 include('includes/menu.php');
 include('includes/header.php');
+
+if($_SESSION["tipoUsuario"]!="Vendedor"){
+  echo '<script type="text/javascript">alert("Inicie sesión nuevamente.");</script>';
+  echo '<script type="text/javascript">onload=window.location="index.php";</script>';
+}else{
 ?>
 
 <style>
@@ -80,3 +85,7 @@ $("#btnVisita").click(function(){
     });
 
 </script>
+
+<?php
+} //cierre llave else SESSION
+?>
