f01709e4f48e1246f7abc5a08f2804756e47e865
MarielaBarba <77119353+MarielaBarba@users.noreply.github.com>
2021-04-19 16:17:53 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,6 @@
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="fontawesome/js/all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.0.2/chart.min.js"></script>
+<script src="js/chart.min.js"></script>
