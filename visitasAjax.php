1fe8ebc29a8565c21cda3c799097e4360666ccb0
MarielaBarba <77119353+MarielaBarba@users.noreply.github.com>
2021-05-25 10:15:52 -0500

    actualizado

--- visitasAjax.php
+++ visitasAjax.php
@@ -309,52 +309,6 @@ if($action=="getPVenta"){
         unset($_SESSION["matrix"]);
 
 //agregar producto si no existe
-}elseif($action == "agregarProducto"){
-    $nombre=(isset($_REQUEST['nombre'])&& $_REQUEST['nombre'] !=NULL)?$_REQUEST['nombre']:'';
-        $segmento=(isset($_REQUEST['segmento'])&& $_REQUEST['segmento'] !=NULL)?$_REQUEST['segmento']:'';
-        $categoria=(isset($_REQUEST['categoria'])&& $_REQUEST['categoria'] !=NULL)?$_REQUEST['categoria']:'';
-        $conteo=(isset($_REQUEST['conteo'])&& $_REQUEST['conteo'] !=NULL)?$_REQUEST['conteo']:'';
-        $precio=(isset($_REQUEST['precio'])&& $_REQUEST['precio'] !=NULL)?$_REQUEST['precio']:'';
-        $procedencia=(isset($_REQUEST['procedencia'])&& $_REQUEST['procedencia'] !=NULL)?$_REQUEST['procedencia']:'';
-        $repetidos=existeProducto($nombre,'',$con);
-        if($repetidos > 0){
-            echo 2;
-        }else{
-            mysqli_query($con,'BEGIN');  
-            $queryInsertProd = mysqli_query($con,"INSERT INTO producto(idCategoria,nombre,segmento,conteo,precio,procedencia) VALUES ('$categoria','$nombre','$segmento', '$conteo', '$precio', '$procedencia')");
-            $idProducto=mysqli_insert_id($con); 
-            $directorio = "imgProductos";
-                if (!file_exists($directorio)) {
-                     mkdir($directorio, 0777, true);
-                }
-             
-            if(isset($_FILES['img']) || $_FILES['img']['size']!=0){
-                $queryInsertImg = mysqli_query($con,"INSERT INTO imgproducto(idProducto) VALUES ($idProducto)");
-                $ultimoId=mysqli_insert_id($con);
-                $nombre = $_FILES['img']['name'];
-                $nombre_tmp = $_FILES['img']['tmp_name'];
-                $partes_nombre = explode('.', $nombre);
-                $extension = end($partes_nombre);
-                $ruta ="imgProductos/";
-    
-                if(move_uploaded_file($nombre_tmp, $ruta.$ultimoId.".".$extension)){
-                    $nombrenuevo = $ultimoId.".".$extension;
-                    $rutabd = "imgProductos/".$nombrenuevo;
-                    $insertRuta = "UPDATE imgproducto SET ruta='$rutabd' WHERE idImgProducto='$ultimoId'";
-                    $queryUpdateImg = mysqli_query($con, $insertRuta);
-                    if($queryInsertProd && $queryInsertImg && $queryUpdateImg ){
-                        mysqli_query($con,'COMMIT');
-                        echo 1;
-                    }else{
-                        mysqli_query($con,'ROLLBACK');
-                        echo 0;  
-                    }
-                  }else{
-                    mysqli_query($con,'ROLLBACK');
-                    echo 0;
-                  }
-              }    
-        }
 }elseif($action=="getCategoria"){
     $queryRes=mysqli_query($con,"SELECT * From categoria");
     echo "<option value=''>Seleccione</option>";
@@ -363,7 +317,6 @@ if($action=="getPVenta"){
        $categoria = $res['categoria'];
       echo "<option value='".$idCategoria."'>$categoria</option>";
    }
-
 }
 
 //Inicia parte de VISITAS ADMIN ***************************************************************************************
